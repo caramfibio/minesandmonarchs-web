e1b2586d7e0edbf8b0eb9b1d5949125fb2c70f65
caramfibio <caracaramfibio@gmail.com>
2026-04-12 21:46:45 +0200

    cartilla mejorada

--- Mundo/Personajes/personaje.js
+++ Mundo/Personajes/personaje.js
@@ -1,3 +1,4 @@
+ import { initializeApp, getApp, getApps }
             from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
         import { getAuth, onAuthStateChanged }
             from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
